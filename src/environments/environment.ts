33e3b9af4af2bdd85551da1bf80e61f3867c44f8
Mario Scrocca <mario.scrocca@gmail.com>
2020-04-01 16:31:11 +0200

    Update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://ns3056488.ip-213-32-26.eu/v2',
+  baseUrl: 'http://localhost:80',
   privacyUrl: 'https://www.cefriel.com/en/privacy'
 };
-
-/*
-local deploy:   baseUrl: 'http://localhost:80'
-
-action deploy:  baseUrl: 'https://survey.actionproject.eu/v2'
-                privacyUrl: 'https://actionproject.eu/privacy-policy/'    
-
-dorothy deploy: baseUrl: 'https://ns3056488.ip-213-32-26.eu/v2'
-                privacyUrl: 'https://www.cefriel.com/en/privacy'
-*/
