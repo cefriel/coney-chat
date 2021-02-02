2f98f445150aaec89392cba251957749c4f4b63e
damiano-sca <damianoscandolari@gmail.com>
2021-02-02 14:21:51 +0100

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://coney.cefriel.com/app',
+  baseUrl: 'http://localhost:80',
   privacyUrl: 'https://www.cefriel.com/en/privacy',
 	enterprise: true
 };
